fb2e2b4bf6686606051bdc301a650a7ab1af308b
Lucas Tavares <lucasr.tavares@outlook.com>
2021-02-24 19:46:45 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/tavares/cursomc/domain/Categoria.java
+++ src/main/java/com/tavares/cursomc/domain/Categoria.java
@@ -2,25 +2,29 @@ package com.tavares.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	
-	public Categoria () {
-		
+
+	public Categoria() {
+
 	}
-	
-	
 
 	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
-
 
 	public Integer getId() {
 		return id;
@@ -37,8 +41,7 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -46,7 +49,6 @@ public class Categoria implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -65,12 +67,9 @@ public class Categoria implements Serializable {
 		return true;
 	}
 
-
 	@Override
 	public String toString() {
 		return "Categoria [id=" + id + ", nome=" + nome + "]";
 	}
-	
-	
-	
+
 }
