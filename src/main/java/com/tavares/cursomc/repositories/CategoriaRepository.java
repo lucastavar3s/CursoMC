f013385ead80a8af6ab747735c34a70079d4be22
Lucas Tavares <lucasr.tavares@outlook.com>
2021-02-24 20:43:01 -0300

    Criando repository e service para categoria
    end
    close
    finish

---
+++ src/main/java/com/tavares/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.tavares.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tavares.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
